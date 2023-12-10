a3b99bf666b0e1d32724dd4aab1529607fc9eb01
Jean Paul Fernandez <jpxoi@icloud.com>
2023-12-10 16:47:49 +0000

    Add small logo for mobile view in Header component

--- src/Containers/Header.jsx
+++ src/Containers/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import logo from '.././assets/logo.png'
+import logo_sm from '.././assets/logo_sm.png'
 
 function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -12,11 +13,15 @@ function Header() {
                     <div>
                         <a href="/#" className="flex items-center space-x-3 rtl:space-x-reverse">
                             <span className="sr-only">Bioeast&Galenos</span>
-                            <img className="h-8 w-auto" src={logo} alt="logo" />
+                            <img className="hidden sm:block h-8 w-auto" src={logo} alt="logo" />
+                            <img className='sm:hidden h-8 w-auto' src={logo_sm} alt='logo' />
                         </a>
                     </div>
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                        <button type="button" className="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Aula Virtual</button>
+                        <button type="button" className="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center">
+                            Aula Virtual
+                        </button>
+
                         <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false" onClick={ () => setMobileMenuOpen(!mobileMenuOpen) }>
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
