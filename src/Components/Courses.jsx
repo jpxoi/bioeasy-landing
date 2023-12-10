0647a3d700673a1a069c1b1993365a433aa4a295
Jean Paul Fernandez <jpxoi@icloud.com>
2023-12-10 11:53:44 +0000

    Update headings in About, Contact, Courses, and Team components

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -1,7 +1,9 @@
 function Courses() {
   return (
     <section className="section__courses bg-white mt-32 mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8" id="cursos">
-        <h1>Cursos</h1>
+        <h1 className="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl">
+            Cursos
+        </h1>
     </section>
   )
 }
--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,7 +1,9 @@
 function Team() {
   return (
     <section className="section__team bg-white mt-32 mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8" id="docentes">
-        <h1>Docentes</h1>
+        <h1 className="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl">
+            Docentes
+        </h1>
     </section>
   )
 }
