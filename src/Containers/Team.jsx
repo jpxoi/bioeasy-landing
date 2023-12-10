0e6a73562e6a252bb89f76ba02bf1f1752f7792d
Jean Paul Fernandez <jpxoi@icloud.com>
2023-12-10 13:26:43 +0000

    Update heading alignment in Team component

--- src/Containers/Team.jsx
+++ src/Containers/Team.jsx
@@ -1,7 +1,7 @@
 function Team() {
   return (
     <section className="section__team bg-white mt-16 mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8" id="docentes">
-        <h1 className="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl">
+        <h1 className="text-center mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl">
             Docentes
         </h1>
     </section>
