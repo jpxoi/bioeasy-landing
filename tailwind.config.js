3bfdd2eb7381b80d51de505994616ae2c2b719c4
Jean Paul Fernandez <jpxoi@icloud.com>
2023-12-10 10:58:38 +0000

    Update tailwind.config.js with content and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    fontFamily: {
+      sans: ['Nunito', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
