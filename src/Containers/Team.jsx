677847f1616c4a6e1cadcf22903e6085e343c389
Jean Paul Fernandez <jpxoi@icloud.com>
2023-12-12 21:37:22 +0000

    Update Team section styling

--- src/Containers/Team.jsx
+++ src/Containers/Team.jsx
@@ -1,6 +1,6 @@
 function Team() {
   return (
-    <section className="section__team bg-white mt-16 mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8" id="docentes">
+    <section className="section section__team bg-white mt-8 mx-auto w-full max-w-screen-xl p-4 py-6 pt-24 lg:py-8 lg:pt-28" id="docentes">
         <h1 className="text-center mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl">
             Docentes
         </h1>
