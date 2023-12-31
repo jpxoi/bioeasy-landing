888831eb2f6d0c56f5ec242bdfca0c5c4342ae63
Jean Paul Fernandez <jpxoi@icloud.com>
2023-12-31 18:42:33 -0500

    Add new course images and update background image URL

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -17,7 +17,7 @@ function ContactForm() {
         <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">
           Mensaje
         </label>
-        <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-teal-500 focus:border-teal-500" placeholder="Escribe más detalles aquí."></textarea>
+        <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-teal-500 focus:border-teal-500" placeholder="Escribe más detalles aquí." minLength="20" required></textarea>
       </div>
       <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-teal-700 sm:w-fit hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300">
         Enviar Mensaje
