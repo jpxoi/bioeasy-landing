361d9e8c524899d5f4013a6da5c52de2a9235313
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-04 10:57:12 -0500

    Update image dimensions in About component

--- src/Containers/About.jsx
+++ src/Containers/About.jsx
@@ -22,7 +22,7 @@ function About() {
                 </p>
             </div>
             <div className="py-8 lg:pt-8 lg:pb-0 px-4 mx-auto max-w-screen-md">
-                <img className="w-full rounded-md" src="img/web_bg_alt.webp" alt="about" />
+                <img className="w-full rounded-md" src="img/web_bg_alt.webp" alt="about" width="1600" height="900" />
             </div>
         </div>
         <div className="about__container py-8 lg:pt-16 lg:pb-0 px-4 mx-auto max-w-screen-xl">
