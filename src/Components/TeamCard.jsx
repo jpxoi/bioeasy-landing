45bd63b0f7656ac645989e3c12991550491c3f61
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-03 22:34:09 -0500

    Update TeamCard styling

--- src/Components/TeamCard.jsx
+++ src/Components/TeamCard.jsx
@@ -2,9 +2,9 @@ function TeamCard(team_member) {
     const { id, name, title, image } = team_member.team_member
 
     return (
-        <div id={id} className="w-full max-w-xs bg-white border border-gray-200 rounded-lg shadow">
+        <div id={id} className="w-full max-w-xs bg-white border border-gray-200 rounded-lg shadow hover:shadow-lg">
             <div className="flex flex-col items-center px-4 pt-4 pb-4">
-                <img className="w-48 h-48 sm:w-44 sm:h-44 lg:w-48 lg:h-48 mb-3 rounded-full shadow-lg" src={ image } alt={ name }/>
+                <img className="w-48 h-48 sm:w-44 sm:h-44 lg:w-48 lg:h-48 mb-3 rounded-full shadow-lg hover:shadow-xl" src={ image } alt={ name }/>
                 <h5 className="mb-1 text-xl font-medium text-gray-900">{ name }</h5>
                 <span className="text-base text-gray-500">{title}</span>
             </div>
