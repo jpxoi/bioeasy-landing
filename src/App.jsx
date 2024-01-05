40fb515e59a50af5c1a35a0136da5257bd9db526
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-04 22:48:07 -0500

    Add react-router-dom and implement 404 error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import Landing from './Pages/Landing'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Landing from './Pages/Landing';
+import Error404 from './Pages/Error404';
 
 function App() {
   return (
-    <>
-      <Landing/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="*" element={<Error404/>}/>
+      </Routes>
+    </Router>
   )
 }
 
