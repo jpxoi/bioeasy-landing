a738c72fbf0e6137acc15ea331f9933925e3234d
Jean Paul Fernandez <jpxoi@icloud.com>
2024-03-27 00:44:22 +0000

    Add Check page and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Error404 from './Pages/Error404';
 import Privacy from './Pages/Privacy';
 import Inscripcion from './Pages/Inscripcion';
 import Success from './Pages/Success';
+import Check from './Pages/Check';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/privacy" element={<Privacy/>}/>
         <Route path="/inscribete" element={<Inscripcion/>}/>
         <Route path="/success" element={<Success/>}/>
+        <Route path="/check" element={<Check/>}/>
         <Route path="*" element={<Error404/>}/>
       </Routes>
     </Router>
