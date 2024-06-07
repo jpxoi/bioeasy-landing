9af636a5bec616cfac70a19d1f73a5494e5e07a0
Jean Paul Fernandez <jpxoi@icloud.com>
2024-06-07 12:26:10 +0100

    Update RegisterForm with reCAPTCHA integration

--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -426,7 +426,7 @@ function RegisterForm({ orderIdentifier }) {
                 </div>
             </div>
 
-            <div className="cf-turnstile" data-sitekey="0x4AAAAAAAcGBs9Tjhc7JWF_" data-theme="light" data-language="es"></div>
+            <div className="g-recaptcha" data-sitekey="6LeiOvMpAAAAAACr7aN04Ao33V7mWBteQCDw8vCA"></div>
 
             <p className="mt-4 mb-8 text-xs font-light text-justify text-gray-500">
                 Considerando la vigencia del Decreto Legislativo Nº 1390 (Restricciones a la difusión de publicidad masiva) y, siendo <strong>Bioeasy Galenos</strong> respetuoso del ordenamiento jurídico vigente, le solicitamos nos brinde su consentimiento para mantenerlo informado acerca de nuestros diferentes servicios a través del envío de nuestra publicidad. La información brindada se utilizará exclusivamente para el envío de publicidad, por lo que se encontrará protegida por la Ley Nº 29733 - Ley de Protección de datos personales.
